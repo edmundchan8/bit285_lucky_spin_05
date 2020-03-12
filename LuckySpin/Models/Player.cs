e99528a54e4a43fecdd20577daead1646760f377
pokedev8 <pokedev8@gmail.com>
2020-03-12 13:05:51 -0700

    Added data annotations to Player Model

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 //TODO: Add the library for Data Annotations
+using System.ComponentModel.DataAnnotations;
+
 namespace LuckySpin.Models
 {
     public class Player
@@ -7,9 +9,12 @@ namespace LuckySpin.Models
         //TODO: Include two annotations for FirstName
         // one that makes it Required (no empty strings)
         // and another that sets its Display prompt to "Your First Name"
+        [Required]
+        [Display (Prompt = "First Name")]
         public string FirstName { get; set; }
         //TODO: Include one annotation for LuckyNumber
         // one that limits its Range between 1 and 9
+        [Range (1, 9)]
         public int LuckyNumber { get; set; }
     }
 }
